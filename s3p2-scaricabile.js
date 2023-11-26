5866cde7f7aac60b07c45b6a85e459b1b5fd4db3
Jonathan Mayr <jonnyfol22@gmail.com>
2023-11-26 11:59:18 +0100

    first commit

---
+++ s3p2-scaricabile.js
@@ -0,0 +1,89 @@
+// Il tuo compito è creare un sito e-commerce per Smartphones. 
+// L'homepage che stai per creare sarà la vetrina dei prodotti disponibili, con qualche informazione per ciascun prodotto. 
+// Non sono richieste funzionalità di carrello/cassa.
+// - Completa gli esercizi dall'1 al 6 SOLAMENTE con HTML
+// - Completa gli altri esercizi con JS
+
+//ESERCIZI IN HTML
+
+// ESERCIZIO 1: Inserisci un tag h1 con il nome del tuo negozio
+// ESERCIZIO 2: Aggiungi una tabella con i 5 prodotti più in vista del tuo negozio
+// ESERCIZIO 3: Aggiungi per ogni prodotto un'immagine, un titolo, una descrizione e un prezzo
+// ESERCIZIO 4: Per ogni elemento della tabella aggiungi un link di Amazon al prodotto esistente
+// ESERCIZIO 5: Aggiungi un footer con il nome e l'indirizzo del tuo negozio
+// ESERCIZIO 6: Aggiungi un campo testuale in cui l'utente può lasciare un commento su un prodotto (al momento non serve inserire nessuna "vera" funzionalità di POST/salvataggio!)
+
+//ESERCIZI IN JS
+// ESERCIZIO 7: Scrivi una funzione per cambiare il contenuto del tag h1 in qualcos'altro
+// ESERCIZIO 8: Scrivi una funzione per cambiare il colore di background della pagina
+// ESERCIZIO 9: Scrivi una funzione per cambiare l'indirizzo presente nel futuro in un altro, fittizio
+// ESERCIZIO 10: Scrivi una funzione per aggiungere una classe CSS ad ogni link Amazon della tabella
+// ESERCIZIO 11: Scrivi una funzione per aggiungere/togliere una classe CSS a tutte le immagini della tabella; questa classe deve modificare la visibilità/invisibilità dell'immagine
+// ESERCIZIO 12: Scrivi una funzione per cambiare il colore del prezzo di ogni prodotto in uno differente, ogni volta che viene invocata
+
+
+//esercizio 7
+
+
+function cambiaH1(modifica) {
+   
+    let ElH1 = document.querySelector('h1');
+  
+    if (ElH1) ElH1.textContent = modifica;
+  }
+  
+ 
+  cambiaH1('Nuovo Cuffie Mania');
+  
+
+//esercizio 8
+  
+function sfondo(color) {
+    let myBody=document.body;
+    if (myBody) {
+        myBody.style.backgroundColor= color;
+        
+    }
+}
+sfondo("green");
+
+
+//esercizo 9
+
+
+let myP = document.getElementById("fakeAdress");
+ myP.innerText = "via Giuseppe, 12";
+
+
+ //esercizio 10
+ let myLink = document.getElementById("link");
+ myLink.classList.add("prova");
+
+ 
+ //esercizio 11
+ function toggleVisibility() {
+    let immagini = document.querySelectorAll('img');
+  
+    immagini.forEach(function(im) {
+      if (im.classList.contains('invisibile')) {
+        im.classList.remove('invisibile');
+      } else {
+        im.classList.add('invisibile');
+      }
+    });
+  }
+  
+
+
+  //12
+
+  function cambiaColorePrezzi() {
+   
+    let prezzi = document.querySelectorAll('.prezzo');
+    let colori = ["red", "blue", "orange", "purple", "brown"];
+    prezzi.forEach(function(prezzo) {
+      var indiceColore = Math.floor(Math.random() * colori.length);
+      prezzo.style.color = colori[indiceColore];
+    });
+  }
+  cambiaColorePrezzi();
